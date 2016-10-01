566e6eee526d07810323429a02898c0262f90469
Cat Allen <catherine.allen@gatech.edu>
2016-10-01 18:03:40 -0400

    Created RegistrationScreen with name, username, and password textfields

---
+++ src/controller/RegistrationScreen.java
@@ -0,0 +1,18 @@
+package controller;/**
+ * Created by Catherine on 10/1/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RegistrationScreen extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
