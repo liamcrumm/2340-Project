c522a02222fa9444956fe41a6f4cd9202cb72644
Sahithi Puligundla <spaqua10@gmail.com>
2016-10-16 17:29:14 -0400

    Created the MapController and connected it to the map view.

---
+++ src/controller/MapController.java
@@ -0,0 +1,69 @@
+package controller;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+import com.lynden.gmapsfx.javascript.object.*;
+
+import fxapp.MainFXApplication;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+
+import netscape.javascript.JSObject;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Sakhi on 10/15/16.
+ */
+public class MapController implements Initializable, MapComponentInitializedListener {
+
+    @FXML
+    private GoogleMapView mapView;
+
+    private GoogleMap map;
+
+    private Window mainStage;
+
+    private MainFXApplication theApp;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        mapView.addMapInializedListener(this);
+    }
+
+    public void setCallbacks(Window stage, MainFXApplication app) {
+        mainStage = stage;
+        theApp = app;
+    }
+
+
+    @Override
+    public void mapInitialized() {
+        MapOptions options = new MapOptions();
+
+        //set up the center location for the map
+        LatLong center = new LatLong(34, -88);
+
+        options.center(center)
+                .zoom(9)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .mapType(MapTypeIdEnum.TERRAIN);
+
+        map = mapView.createMap(options);
+
+
+    }
+}
