52bd7fb2cd302d8e3415ccc5c21bba03807a3dc8
Sahithi Puligundla <spaqua10@gmail.com>
2016-10-18 17:49:13 -0400

    Added the pins / markers part to the Map Controller but as of now they are not showing up.

--- src/controller/MapController.java
+++ src/controller/MapController.java
@@ -2,10 +2,9 @@ package controller;
 
 
 import com.lynden.gmapsfx.MapComponentInitializedListener;
-import com.lynden.gmapsfx.javascript.object.GoogleMap;
-import com.lynden.gmapsfx.javascript.object.LatLong;
-import com.lynden.gmapsfx.javascript.object.MapOptions;
-import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.oracle.tools.packager.Log;
 import fxapp.MainFXApplication;
 
 import javafx.fxml.FXML;
@@ -15,10 +14,13 @@ import javafx.stage.Window;
 import com.lynden.gmapsfx.GoogleMapView;
 
 
+import model.AccountsManager;
+import model.Report;
 import netscape.javascript.JSObject;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -66,6 +68,35 @@ public class MapController implements Initializable, MapComponentInitializedList
                 .mapType(MapTypeIdEnum.TERRAIN);
 
         map = mapView.createMap(options);
+
+        /** now we communciate with the model to get all the locations for markers */
+        AccountsManager ac = LoginController.accounts;
+        ArrayList<Report> reports = ac.getReportsList();
+
+        for (Report r: reports) {
+            MarkerOptions markerOptions = new MarkerOptions();
+            LatLong loc = new LatLong(r.getLat(), r.getLong());
+            String title = "Report " + r.getReportNumber();
+            String description = "Water Type: " + r.getType() + "\nWater Condition: " + r.getCondition();
+
+            markerOptions.position(loc)
+                    .visible(Boolean.TRUE)
+                    .title(title);
+
+            Marker marker = new Marker(markerOptions);
+
+            map.addUIEventHandler(marker,
+                    UIEventType.click,
+                    (JSObject obj) -> {
+                        InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+                        infoWindowOptions.content(description);
+
+                        InfoWindow window = new InfoWindow(infoWindowOptions);
+                        window.open(map, marker);
+                    });
+
+            map.addMarker(marker);
+        }
     }
 
     /**
