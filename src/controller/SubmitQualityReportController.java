c6ace62c090cc96adeafaf4a091fa64356871ffc
kburke61 <kburke@sssmri.com>
2016-10-27 12:59:11 -0400

    Adjust button handling for Submit Quality Report.

--- src/controller/SubmitQualityReportController.java
+++ src/controller/SubmitQualityReportController.java
@@ -60,7 +60,9 @@ public class SubmitQualityReportController {
         BooleanBinding booleanBind = nameField.textProperty().isEmpty()
                 .or(timeField.textProperty().isEmpty())
                 .or(latitudeField.textProperty().isEmpty())
-                .or(longitudeField.textProperty().isEmpty());
+                .or(longitudeField.textProperty().isEmpty())
+                .or(virusField.textProperty().isEmpty())
+                .or(contaminantField.textProperty().isEmpty());
         saveButton.disableProperty().bind(booleanBind);
     }
 
