a396774d3ee92580024d512d080b2a5d0da96335
Sahithi Puligundla <spaqua10@gmail.com>
2016-11-01 14:28:08 -0400

    Added the make graph method implementation. Changed some of the fx ids in the history graph fxml file. I do not know if it works yet.

--- src/controller/HistoryGraphController.java
+++ src/controller/HistoryGraphController.java
@@ -1,10 +1,22 @@
 package controller;
 
 import fxapp.MainFXApplication;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import model.AccountsManager;
+import model.QualityReport;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 /**
  * Created by Kimberly Burke on 10/29/2016.
@@ -22,6 +34,9 @@ public class HistoryGraphController {
     @FXML private ComboBox locationComboBox;
     @FXML private ComboBox yearComboBox;
     @FXML private Button makeButton;
+    @FXML private NumberAxis numAxis;
+    @FXML private CategoryAxis monthAxis;
+    @FXML private ScatterChart<CategoryAxis,NumberAxis> historyGraph;
 
     ToggleGroup type = new ToggleGroup();
 
@@ -41,6 +56,19 @@ public class HistoryGraphController {
         virusButton.setToggleGroup(type);
         contaminantButton.setToggleGroup(type);
         virusButton.setSelected(true); // set default selection
+
+        //add the locations and years as dropdown options in the corresponding combo boxes
+        ObservableList<String> locations = FXCollections.observableArrayList();
+        ObservableList<Integer> years = FXCollections.observableArrayList();
+        ArrayList<QualityReport> qreps = account.getQualityReportsList();
+        for(QualityReport r: qreps) {
+            String loc = r.getLat() + "," + r.getLong();
+            locations.add(loc);
+            int year = r.getDate().getYear();
+            years.add(year);
+        }
+        locationComboBox.setItems(locations);
+        yearComboBox.setItems(years);
     }
 
     /**
@@ -77,6 +105,74 @@ public class HistoryGraphController {
     public void handleMakeGraphPressed() {
         boolean successful = false; // needed for closing dialog box correctly
         // TODO: need a check for when location and year entry empty
+        String loc = locationComboBox.getSelectionModel().getSelectedItem().toString();
+        String year = yearComboBox.getSelectionModel().getSelectedItem().toString();
+        if((loc == null && year == null) || (loc == null) || (year == null)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Graph cannot be created");
+            alert.setHeaderText("There is not enough information");
+            alert.setContentText("Both a year and a location must be entered in order for the graph to be created.");
+            alert.showAndWait();
+        } else {
+            //change the selected strings to int and doubles for the year and lat,long.
+            int yearSelected = Integer.parseInt(year);
+            String[] parts = loc.split(",");
+            double lat = Double.parseDouble(parts[0]);
+            double longitude = Double.parseDouble(parts[1]);
+
+            ObservableList<String> months = FXCollections.observableArrayList();
+            months.addAll("January","February","March","April","May","June","July","August","September","October"
+                    ,"November","December");
+            monthAxis.setCategories(months);
+
+            //Get the selected ppm value(virus or contaminant)
+            RadioButton selectedPPM = (RadioButton) type.getSelectedToggle();
+            String ppm = selectedPPM.getText();
+
+            //Get the reports that match the year and location given
+            //Map<String,Integer> graphPoints = new HashMap<String,Integer>();
+            ObservableList<XYChart.Data<String,Integer>> data = FXCollections.observableArrayList();
+            ArrayList<QualityReport> allReports = account.getQualityReportsList();
+            //Make the graph according to the ppm that is given
+            if(ppm.equals("Virus")) {
+                for(QualityReport r: allReports) {
+                    double repLat = r.getLat();
+                    double repLong = r.getLong();
+                    int repYear = r.getDate().getYear();
+                    if(repLat == lat && repLong == longitude && repYear == yearSelected) {
+                        //graphPoints.put(r.getDate().getMonth().toString(),r.getVirus());
+                        String month = r.getDate().getMonth().toString();
+                        int virus = r.getVirus();
+                        XYChart.Data<String,Integer> point = new XYChart.Data<String,Integer>(month,virus);
+                        data.add(point);
+
+                    }
+                }
+                XYChart.Series series = new XYChart.Series(data);
+                historyGraph.getData().addAll(series);
+
+            } else {
+                for(QualityReport r: allReports) {
+                    double repLat = r.getLat();
+                    double repLong = r.getLong();
+                    int repYear = r.getDate().getYear();
+                    if(repLat == lat && repLong == longitude && repYear == yearSelected) {
+                        //graphPoints.put(r.getDate().getMonth().toString(),r.getVirus());
+                        String month = r.getDate().getMonth().toString();
+                        int cont = r.getContaminant();
+                        XYChart.Data<String,Integer> point = new XYChart.Data<String,Integer>(month,cont);
+                        data.add(point);
+
+                    }
+                }
+                XYChart.Series series = new XYChart.Series(data);
+                historyGraph.getData().addAll(series);
+
+            }
+
+
+
+        }
         _okClicked = true;
         if(successful) {
             _dialogStage.close();
